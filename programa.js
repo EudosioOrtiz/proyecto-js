006aef6ef7ad67c75602cf32bfe2ed69d0be10bd
Eudosio Ortiz Guillen <163951147+EudosioOrtiz@users.noreply.github.com>
2024-03-21 09:11:15 -0600

    we added the variables khowledge and some special values and definitions about them

--- programa.js
+++ programa.js
@@ -6,6 +6,20 @@ var miVariable = 23;
 
 console.log(miVariable);
 
-var primer;
-let segundo;
-const tercero = 0;
+//alert() funcion para desplegar una alerta en la pagina
+
+var primer; // se puede definir o no, var se puede declarar mas de una vez, var se puede usar como global si esta afuera de una funcion y como local adentro de una funcion
+            // var se puede usar dentro y fuera de condicionales
+let segundo; //se puede definir o no cuando se inicializa pero puede ser undefined, pero no se puede declarar mas de una vez, si se puede reasignar el valor
+            // let no se puede usar dentro y fuera de condicionales
+const tercero = 0;// las const ya no pueden cambiar su valor y se tiene que declarar cuando se inicializa
+
+let numero=1, numero2=2 ,numero3=3;
+
+//el hoisting es el concepto de declarar un variable despues de su uso
+
+let nulo = null; //valores especiales, Nan significa Not a Number
+
+let nombre = prompt("dame un nombre") //prompt es in input que se puede almacenar en variables
+
+alert(nombre)
