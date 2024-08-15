ef8fe9553e7ec41d68b2d5df4e696e9ea3ae83d9
EudosioOrtiz <163951147+EudosioOrtiz@users.noreply.github.com>
2024-08-15 16:36:35 -0600

    new topics

--- Capitulo15/Capitulo15DragAndDrop.js
+++ Capitulo15/Capitulo15DragAndDrop.js
@@ -3,7 +3,10 @@
 const circulo = document.querySelector(".circulo")
 const rectangulo = document.querySelector(".rectangulo")
 
-circulo.addEventListener("dragstart", ()=> console.log(1));
+circulo.addEventListener("dragstart", (e)=>{
+    e.dataTransfer.setData("clase", e.target.className)
+    console.log(e.dataTransfer.getData("clase"))
+});
 circulo.addEventListener("drag", ()=> console.log(2));
 circulo.addEventListener("dragend", ()=> console.log(3));
 
@@ -13,5 +16,28 @@ rectangulo.addEventListener("dragover",(e)=>{
     e.preventDefault();
     console.log(2)
 })
-rectangulo.addEventListener("drop",()=>console.log(3))
+rectangulo.addEventListener("drop",(e)=>{
+    console.log(e.dataTransfer.getData("clase"))
+    console.log(3)
+})
 rectangulo.addEventListener("dragleave",()=>console.log(4))
+
+//ejemplo
+
+const zona = document.querySelector(".zona");
+zona.addEventListener("dragover",(e)=>{
+    e.preventDefault();
+})
+
+zona.addEventListener("drop",(e)=>{
+    let n = e.dataTransfer.getData("textura");
+    zona.style.background = `url("textu${n}.jpg")`
+})
+
+for (let i = 1; i < document.querySelector(".texturas").children.length + 1 ; i++) {    
+    document.querySelector(`.textura${i}`).addEventListener("dragstart",(e)=>trasnferirTextura(i,e))    
+}
+
+const trasnferirTextura = (n,e) =>{
+    e.dataTransfer.setData("textura",n);
+}
